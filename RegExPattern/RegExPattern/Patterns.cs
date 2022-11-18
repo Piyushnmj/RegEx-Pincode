77755395a1d48f1eef40adae4cdd9d1d9dd0fc8a
Piyushnmj <piyushnmj@yahoo.com>
2022-11-18 20:17:35 +0530

    6 Digit Postal Code

---
+++ RegExPattern/RegExPattern/Patterns.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExPattern
+{
+    public class Patterns
+    {
+
+        public void ValidatePinCode(string pincode)
+        {
+            var regex = new Regex("^[1-9][0-9]{5}$");
+            bool matchRes = regex.IsMatch(pincode);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"\n{pincode} Matches");
+            }
+            else
+            {
+                Console.WriteLine($"\n{pincode} does not match");
+            }
+        }
+    }
+}
--- RegExPattern/RegExPattern/Program.cs
+++ RegExPattern/RegExPattern/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expressions Problems");
+
+            Patterns objPattern = new Patterns();
+            objPattern.ValidatePinCode("400088");
         }
     }
 }
